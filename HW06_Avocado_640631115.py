669348184d5f3ea8e20a03bbfaf95df4566d50e7
Chomchanok <chomchanok_y@cmu.ac.th>
2021-09-04 17:43:31 +0700

    add comments for task 1

--- HW06_Avocado_640631115.py
+++ HW06_Avocado_640631115.py
@@ -4,9 +4,9 @@ df = pd.read_csv("avocado.csv")
 df.head()
 
 ## 1.1 Which region sold the largest amount of avocado ?
-sum_each_region = df.groupby(['region'],sort=False)[['Total Volume']].sum()
-max = sum_each_region.max()
-large = sum_each_region['Total Volume'].idxmax()
+sum_each_region = df.groupby(['region'],sort=False)[['Total Volume']].sum() # group by each region to calculate total amount of avocado
+max = sum_each_region.max() # find max value
+large = sum_each_region['Total Volume'].idxmax() # find max index of max value
 print(" The region which sold the largest amount of avocado is {} which is {}$".format(large,max.values))
 
 
