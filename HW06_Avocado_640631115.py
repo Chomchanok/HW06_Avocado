5b4bbb78a1ff5410a927e061ef20068fb2abfb64
Chomchanok <chomchanok_y@cmu.ac.th>
2021-09-04 18:06:19 +0700

    finish task 3

--- HW06_Avocado_640631115.py
+++ HW06_Avocado_640631115.py
@@ -22,3 +22,20 @@ max_lot_4770 = lot_4770.max() # find max value of PLU 4770
 combinemax = max_lot_4046.append([max_lot_4225,max_lot_4770]) # To combine max values from each PLUs
 print("The biggest lot of sold avocado came from : ",combinemax.idxmax()) 
 
+## 2.1 Which region sold the smallest amount of avocado ?
+min = sum_each_region.min() # find min value
+small = sum_each_region['Total Volume'].idxmin() # find max index of max value
+print(" The region which sold the smallest amount of avocado is {} which is {}$".format(small,min.values))
+
+## 2.2 In this region, where the biggest lot of sold avocado came from (4046, 4225, 4770) ?
+min_lot_4046 = lot_4046.min()# find min value of PLU 4046 
+min_lot_4225 = lot_4225.min()# find min value of PLU 4225
+min_lot_4770 = lot_4770.min()# find min value of PLU 4770
+
+combinemin = min_lot_4046.append([min_lot_4225,min_lot_4770]) # To combine min values from each PLUs
+print("The biggest lot of sold avocado came from : ",combinemin.idxmax())
+
+## 3. Which region sold the highest price of avocado in average ?
+region_avg_price = df.groupby(['region'],sort=False)[['Total Volume']].mean() # group by each region to calculate average price of avocado 
+highest_avg_price = region_avg_price['Total Volume'].idxmax() # find max indext of highest price 
+print("The region which sold the highest price of avocado in average is : ",highest_avg_price )
